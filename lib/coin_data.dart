07732549255c024e339610ce65b55d9fdb661b20
Lucas GILHARD <l.gilhard@gmail.com>
2020-03-12 17:01:21 +0100

    fetch exchange rates from coinAPI

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,19 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+//const coinAPIKey = '80D4B64F-BF8A-46AF-BAF6-A11AD9E67AEF';
+const coinAPIKey = '4A45B086-0F1F-4C91-A682-535350AAEF0E';
+const coinAPIUrl = 'https://rest.coinapi.io/v1/';
+
+class CoinData {
+  Future<String> getExchangeRate(String crypto, String currency) async {
+    var res = await http
+        .get('${coinAPIUrl}exchangerate/$crypto/$currency?apikey=$coinAPIKey');
+    if (res.statusCode == 200) {
+      return (jsonDecode(res.body)['rate']).toStringAsFixed(2);
+    } else {
+      print(res.body);
+      return '0.0';
+    }
+  }
+}
